7f287dffa0a157b3f00525b87abaefc704647ed0
bhushansable <bhushansable9876@gmail.com>
2026-01-09 10:17:10 +0530

    Add initial implementation of Tic-Tac-Toe game with basic functionality

---
+++ app.js
@@ -0,0 +1,51 @@
+
+
+let boxes = document.querySelectorAll(".btn");
+let winnerShow = document.querySelector("#winner");
+
+let player =true;
+
+let gamePatterns = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
+
+const disable =()=>{
+    for(let box of boxes){
+        box.disabled = true
+    }
+}
+
+const showWin =(winner)=>{
+    winnerShow.innerText=`Winner is ${winner}`
+    disable();
+    
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(player){
+            box.innerText ="O"
+            box.style.color ="red"
+            player=false
+        }else{
+            box.innerText ="X"
+             box.style.color ="yellow"
+            player=true  
+        }
+    box.disabled =true
+    win();
+    })
+})
+
+win =()=>{
+    gamePatterns.forEach((pattern)=>{
+        let p1 =(boxes[pattern[0]].innerText)
+        let p2 =(boxes[pattern[1]].innerText)
+        let p3 =(boxes[pattern[2]].innerText)
+        if(p1 !="" && p2!=""&& p3 !=""){
+            if(p1 === p2 && p2 ===p3){
+                console.log("winner")
+                 showWin(p1)
+            }
+        
+        }})
+    
+}
